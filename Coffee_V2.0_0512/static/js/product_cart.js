48acecf0b7f5d69e9b894fdcbf3ae246735cae69
zqh1225 <159679005+zqh1225@users.noreply.github.com>
2024-05-17 20:02:55 +0800

    1.完成cart.html页面的修改 2.完成index_black.html的cartbar部分修改 (#7)

--- Coffee_V2.0_0512/static/js/product_cart.js
+++ Coffee_V2.0_0512/static/js/product_cart.js
@@ -14,7 +14,7 @@ function minus(index) {
   var count = parseInt(amounts[index].value); //数量加1
 
   if (count <= 1) {
-    alert("不能再减了，快没了！！");
+    alert("The quantity of the product cannot be less than 1 piece!");
   } else {
     //得到第一个amount的元素的value属性的值
     var count = parseInt(amounts[index].value) - 1; //数量加1
@@ -26,7 +26,7 @@ function minus(index) {
     //乘以Math.pow(10,2)的原因为避免失真
     var totalMoney = ((price * Math.pow(10, 2)) * count) / Math.pow(10, 2);
 
-    document.getElementById("price" + index).innerHTML = "$" + totalMoney;
+    document.getElementById("price" + index).innerHTML = "$" + totalMoney.toFixed(1);
   }
 
   total();
@@ -53,7 +53,7 @@ function plus(index) {
   var totalMoney = ((price * Math.pow(10, 2)) * count) / Math.pow(10, 2);
 
   //把当前价格显示在文本中
-  document.getElementById("price" + index).innerHTML = "$" + totalMoney;
+  document.getElementById("price" + index).innerHTML = "$" + totalMoney.toFixed(1);
 
   total();
 }
@@ -78,19 +78,24 @@ function total() {
   }
 
   //把总金额显示再指定的元素中
-  document.getElementById("totalPrice").innerHTML = "$" + sumMoney;
+  document.getElementById("totalPrice").innerHTML = "$ " + sumMoney.toFixed(1);
 
 }
 
 //删除
 function deleteRow(element) {
-  if (confirm("确认要删除吗？")) {
+  if (confirm("Are you sure you want to delete it?")) {
     // 找到元素的父级 <tr> 元素
     var row = element.parentNode.parentNode;
     row.parentNode.removeChild(row);
-    alert("删除成功！！");
+    alert("Successfully deleted!");
   }
+  total();
 }
+
+window.onload = function () {
+  total();
+};
 
 
 
