d603507fe820b6ce22872db3843e53bbdbc56078
zqh1225 <159679005+zqh1225@users.noreply.github.com>
2024-05-23 18:50:46 +0800

    尝试修改shop_details的逻辑，尝试成功，merge之后再继续修改 (#12)

---
+++ Coffee_V2.0_0512/static/js/shop_details.js
@@ -0,0 +1,5 @@
+function updateProductInfo(name, price, description) {
+  document.getElementById('product-name').innerText = name;
+  document.getElementById('product-price').innerText = price;
+  document.getElementById('product-description').innerText = description;
+}
